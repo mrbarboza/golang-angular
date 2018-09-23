ebfdf6d2b389515cddd4d311a2487aff83c78dee
mrbarboza <sagazman@gmail.com>
2018-09-23 13:46:51 -0300

    Golang & Angular - Pt. 2

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+import dotenv from "dotenv";
+
+const config = dotenv.config();
+
 export const environment = {
-  production: true
+  production: true,
+  gateway: "",
+  callback: "http://localhost:4200/callback",
+  domain: "https://" + process.env.AUTH0_DOMAIN + "/",
+  clientId: process.env.AUTH0_CLIENTID,
+  audience: process.env.AUTH0_API_IDENTIFIER
 };
